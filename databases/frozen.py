cd588edcfec1f38d69cfef1d640b2d135d95ba8a
Juan B Cabral <jbc.develop@gmail.com>
2020-03-24 22:25:49 -0300

    frozeneando la db en github

---
+++ databases/frozen.py
@@ -0,0 +1,12 @@
+import requests
+
+URLS = {
+    "cases.xlsx": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfinng5SDBH9RSJMHJk28dUlW3VVSuvqaBSGzU-fYRTVLCzOkw1MnY17L2tWsSOppHB96fr21Ykbyv/pub?output=xls"
+}
+
+for fn, url in URLS.items():
+    print(f"DOWNLOADING '{fn}'...")
+    r = requests.get(url, allow_redirects=True)
+
+    with open(fn, 'wb') as fp:
+        fp.write(r.content)
