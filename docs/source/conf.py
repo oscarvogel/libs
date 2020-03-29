9c19b5cc770c43ad75f863419b883d26f7f501e7
Bruno SANCHEZ <bruno.sanchez@duke.edu>
2020-03-29 15:21:03 -0400

    Tweaked the conf a bit. Different theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,15 +51,22 @@ language = 'es'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['build', 
+                    'source/.ipynb_checkpoints/*',
+                    'source/tutorial/.ipynb_checkpoints/*']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst', '.ipynb']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
