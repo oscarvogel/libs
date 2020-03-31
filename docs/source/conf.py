0cab28ae6920563a7f3cdd8b9ed15123ded4754b
Juan B Cabral <jbc.develop@gmail.com>
2020-03-31 02:20:24 -0300

    docs test done

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'nbsphinx'
+    'nbsphinx',
+    'IPython.sphinxext.ipython_console_highlighting'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,9 +52,11 @@ language = 'es'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['build', 
-                    'source/.ipynb_checkpoints/*',
-                    'source/tutorial/.ipynb_checkpoints/*']
+exclude_patterns = [
+    'build',
+    '**.ipynb_checkpoints',
+    'source/.ipynb_checkpoints/*',
+    'source/tutorial/.ipynb_checkpoints/*']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -66,7 +69,7 @@ source_suffix = ['.rst', '.ipynb']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme' 
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
